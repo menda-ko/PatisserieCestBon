132f334334c22092a8a0520176917ee603101fe0
大谷　実央 <qqqgladosppp@gmail.com>
2020-06-13 23:08:04 +0900

    プロジェクト ファイルを追加します。

---
+++ PatisserieCestBon/Controllers/StockController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PatisserieCestBon.Controllers
+{
+    public class StockController : Controller
+    {
+        // GET: Stock
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PatisserieCestBon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PatisserieCestBon.MvcApplication" Language="C#" %>
