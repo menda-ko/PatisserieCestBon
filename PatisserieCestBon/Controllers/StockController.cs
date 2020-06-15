fdf120efff5073ba4b7f387af29a94173b2c573b
光一 砂邊 <sunabe.koichi@ntsol.co.jp>
2020-06-15 17:12:00 +0900

    在庫情報管理一覧編集

--- PatisserieCestBon/Controllers/StockController.cs
+++ PatisserieCestBon/Controllers/StockController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PatisserieCestBon.Models;
 
 namespace PatisserieCestBon.Controllers
 {
     public class StockController : Controller
     {
         // GET: Stock
-        public ActionResult Index()
+        public ActionResult StockList()
         {
-            return View();
+            using (var db = new DatabaseEntities())
+            {
+                var ul = db.Stocks.ToList();
+                return View(ul);
+            }
+        }
+        public ActionResult StockUpdate1(Stock stock)
+        {
+            using(var db = new DatabaseEntities())
+            {
+                var u = db.Stocks.Find(stock.itemNo);
+                ViewBag.stock = u;
+                return View(u);
+            }
+        }
+        public ActionResult StockUpdate2(Stock stock)
+        {
+            using(var db = new DatabaseEntities())
+            {
+                var u = db.Stocks.Find(stock.itemNo);
+                u.stock1 = stock.stock1;
+                db.SaveChanges(); 
+                return Redirect("Stock/StockList");
+            }
         }
     }
 }
