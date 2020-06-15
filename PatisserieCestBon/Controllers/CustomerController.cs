19484e40da366a28a7522789341fee97e35a19a5
政紀 日下 <kusaka.masaki@ntsol.co.jp>
2020-06-15 14:24:19 +0900

    顧客の新規登録仮完成

--- PatisserieCestBon/Controllers/CustomerController.cs
+++ PatisserieCestBon/Controllers/CustomerController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using PatisserieCestBon.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +11,48 @@ namespace PatisserieCestBon.Controllers
 {
     public class CustomerController : Controller
     {
-        // GET: Customer
-        public ActionResult Index()
+        
+        public ActionResult CustomerAdd1()
         {
             return View();
+        }
+
+        public ActionResult CustomerAdd2(string companyName, string address, string telNo, string customerName, string customerKana, string dept, string email, string password)
+        {
+            ViewBag.companyName = companyName;
+            ViewBag.address = address;
+            ViewBag.telNo = telNo;
+            ViewBag.customerName = customerName;
+            ViewBag.customerKana = customerKana;
+            ViewBag.dept = dept;
+            ViewBag.email = email;
+            ViewBag.password = password;
+            return View();
+        }
+
+        public ActionResult CustomerAdd3(string companyName, string address, string telNo, string customerName, string customerKana, string dept, string email, string password)
+        {
+
+            using (var db = new DatabaseEntities())
+            {
+                Customer model = new Customer
+                {
+                    companyName = companyName,
+                    address = address,
+                    telNo = telNo,
+                    customerName = customerName,
+                    customerKana = customerKana,
+                    dept = dept,
+                    email = email,
+                    password = password
+                };
+                db.Customers.Add(model);
+                //文字数？によっては動かない場合あり
+                db.SaveChanges();
+                ViewBag.model = model;
+                ViewBag.customerId = model.customerId;
+                return View();
+            }
         }
     }
 }
