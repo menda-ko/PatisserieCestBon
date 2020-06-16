bc804f585685bbe9935714d2d9f2d98bb7a38d8c
大谷　実央 <qqqgladosppp@gmail.com>
2020-06-16 09:46:16 +0900

    ▽商品追加登録機能 ・Add1～Add3メソッド追加（入力値チェックはいったん寝かしとく） ・↑に合わせてViews/ItemにAdd1・Add2を追加 　エラーメッセージ表示はコメントアウト ▽商品一覧表示機能 ・Views/Item/List 　formタグの開始をtableタグの前に移動

--- PatisserieCestBon/Controllers/ItemController.cs
+++ PatisserieCestBon/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
-﻿using PatisserieCestBon.Models;
+﻿using Microsoft.Ajax.Utilities;
+using PatisserieCestBon.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ namespace PatisserieCestBon.Controllers
     public class ItemController : Controller
     {
         private DatabaseEntities db = new DatabaseEntities();
-        // 商品一覧表示（バックオフィス）
+        // ★商品一覧表示（バックオフィス）ここから★
         public ActionResult List()
         {
             // Itemテーブルから、削除フラグがFalseのもののみ取得
@@ -19,29 +20,88 @@ namespace PatisserieCestBon.Controllers
             ViewBag.ItemList = itemList;
             return View();
         }
+        // ★商品一覧表示（バックオフィス）ここまで★
 
-        // 商品追加登録機能（Add1～Add3）
+        // ★商品追加登録（Add1～Add3）ここから★
         // Add1 … 商品追加入力画面へ
         public ActionResult Add1()
         {
             return View();
         }
-        /* Add2 … 商品追加確認画面へ
-        public ActionResult Add2(int itemNo, string itemName,
-            string size, string photoUrl, string unitPrice,
+        // Add2 … 商品追加確認画面へ
+        public ActionResult Add2(int itemNo, string itemName, string size, string photoUrl, int unitPrice,
             string assortment, string category)
         {
-            /*            if (itemNo == null)
-                        {
-                            ViewBag.ErrorMessage = PatisserieCestBon.Properties.Settings.Default.p032_error_RecuiredItemNo;
-                        }
-                        if (itemName == null)
-                        {
-                            ViewBag.ErrorMessage = PatisserieCestBon.Properties.Settings.Default.p032_error_RecuiredItemName
-                        }
-                        return View();
-                    } 
+            /* // エラーメッセージを格納するリスト
+            List<string> errorMessageList = new List<string>();
+            // 必須項目が入力されていない場合「～を入力してください」のエラーメッセージをリストに格納
+            if (itemNo == null)
+            {
+                errorMessageList.Add(PatisserieCestBon.Properties.Settings.Default.p032_error_RecuiredItemNo);
+            }
+            if (string.IsNullOrWhiteSpace(itemName))
+            {
+                errorMessageList.Add(PatisserieCestBon.Properties.Settings.Default.p032_error_RecuiredItemName);
+            }
+            if (string.IsNullOrWhiteSpace(size))
+            {
+                errorMessageList.Add(PatisserieCestBon.Properties.Settings.Default.p032_error_RecuiredSize);
+            }
+            if (string.IsNullOrWhiteSpace(photoUrl)) // photoUrlは必須？NULL可？
+            {
+                errorMessageList.Add(PatisserieCestBon.Properties.Settings.Default.p032_error_RecuiredPhotoUrl);
+            }
+            if (string.IsNullOrWhiteSpace(unitPrice))
+            {
+                errorMessageList.Add(PatisserieCestBon.Properties.Settings.Default.p032_error_RecuiredUnitPrice);
+            }
+            if (errorMessageList != null)
+            {
+                // エラーメッセージリストがnullでない場合、画面遷移せずエラーメッセージを表示
+                    foreach (var errorMessage in errorMessageList)
+                {
+                    ViewBag.ErrorMessageList = errorMessage;
+                };
+                return View("Add1");
+            }
+            */
+            // 確認画面に表示するために値を渡す
+            var item = new Item()
+            {
+                itemNo = itemNo
+                , itemName = itemName
+                , size = size
+                , photoUrl = photoUrl
+                , unitPrice = unitPrice
+                , assortment = assortment
+                , category = category
+            };
+            ViewBag.model = item;
+            Session["newItem"] = item;
             return View();
-    }*/
+        }
+        // Add3 … Itemテーブルに商品を登録
+        public ActionResult Add3(int itemNo, string itemName, string size, string photoUrl, int unitPrice,
+            string assortment, string category)
+        {
+                var item = new Item()
+                {
+                    itemNo = itemNo
+                    ,itemName = itemName
+                    ,size = size
+                    ,photoUrl = photoUrl
+                    ,unitPrice = unitPrice
+                    ,assortment = assortment
+                    ,category = category
+                };
+                db.Items.Add(item);
+                db.SaveChanges();
+                ViewBag.InfoMessage = PatisserieCestBon.Properties.Settings.Default.p031_info_AddSuccess;
+            var itemList = db.Items
+                .Where(i => i.deleteFlag.Equals(false));
+            ViewBag.ItemList = itemList;
+            return View("List");
+            }
+        // ★商品追加登録（Add1～Add3）ここまで★
     }
 }
