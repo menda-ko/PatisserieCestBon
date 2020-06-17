0c0d26622a0be5b86867975912a8a2563cecc074
光一 砂邊 <sunabe.koichi@ntsol.co.jp>
2020-06-17 13:52:47 +0900

    顧客IDをもとにステータス確認機能追加

--- PatisserieCestBon/Controllers/OrderController.cs
+++ PatisserieCestBon/Controllers/OrderController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PatisserieCestBon.Models;
 
 namespace PatisserieCestBon.Controllers
 {
     public class OrderController : Controller
     {
         // GET: Order
-        public ActionResult Index()
+        public ActionResult OrderCancel1()
+        {
+
+            return View();
+        }
+        public ActionResult OrderCancel2()
         {
             return View();
+        }
+        public ActionResult StatusCheck()
+        {
+            using (var db = new DatabaseEntities())
+            {
+                var ul = db.OrderInfoes.Where(e => e.customerId == 1);
+                var list = ul.ToList();
+
+                return View(list);
+            }
         }
     }
 }
