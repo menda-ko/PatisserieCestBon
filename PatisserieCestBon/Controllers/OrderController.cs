59bd60c36a842098ea954838073b4f031f9bf956
myanano <kusaka19990123@gmail.com>
2020-06-22 16:45:37 +0900

    新規顧客登録のログイン画面へ戻る時のパスを修正 ViewのLayout部分を修正 見積確認と発注確定を実装(未完成)

--- PatisserieCestBon/Controllers/OrderController.cs
+++ PatisserieCestBon/Controllers/OrderController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using PatisserieCestBon.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +11,19 @@ namespace PatisserieCestBon.Controllers
 {
     public class OrderController : Controller
     {
-        // GET: Order
-        public ActionResult Index()
+        //cartListの型が分からない
+        public ActionResult Add(int subTotal, int tax, int total)
         {
+            // セッション確認
+            if (Session["loginUserName"] == null)
+            {
+                // セッションが空だったらシステムエラー
+                return Redirect("CustomerError");
+            }
+
+            ViewBag.subTotal = subTotal;
+            ViewBag.tax = tax;
+            ViewBag.total = total;
             return View();
         }
     }
