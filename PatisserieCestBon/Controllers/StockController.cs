18b30710ed21e4e74f8014e6b771a62743906df3
光一 砂邊 <sunabe.koichi@ntsol.co.jp>
2020-06-22 17:00:43 +0900

    受注検索、受注更新、在庫管理、ステータス管理追加

--- PatisserieCestBon/Controllers/StockController.cs
+++ PatisserieCestBon/Controllers/StockController.cs
@@ -12,29 +12,86 @@ namespace PatisserieCestBon.Controllers
         // GET: Stock
         public ActionResult StockList()
         {
+            // セッション確認
+            if (Session["loginUserName"] == null)
+            {
+                // セッションが空だったらシステムエラー
+                return Redirect("EmployeeError");
+            }
             using (var db = new DatabaseEntities())
             {
                 var ul = db.Stocks.ToList();
+                ViewBag.stock = ul;
                 return View(ul);
             }
         }
         public ActionResult StockUpdate1(Stock stock)
         {
-            using(var db = new DatabaseEntities())
+            // セッション確認
+            if (Session["loginUserName"] == null)
+            {
+                // セッションが空だったらシステムエラー
+                return Redirect("EmployeeError");
+            }
+            using (var db = new DatabaseEntities())
             {
                 var u = db.Stocks.Find(stock.itemNo);
                 ViewBag.stock = u;
                 return View(u);
             }
         }
-        public ActionResult StockUpdate2(Stock stock)
+        public ActionResult StockUpdate2(Stock stock,string receipt, string decrease, string update)
         {
-            using(var db = new DatabaseEntities())
+            // セッション確認
+            if (Session["loginUserName"] == null)
             {
-                var u = db.Stocks.Find(stock.itemNo);
-                u.stock1 = stock.stock1;
-                db.SaveChanges(); 
-                return Redirect("Stock/StockList");
+                // セッションが空だったらシステムエラー
+                return Redirect("EmployeeError");
+            }
+            using (var db = new DatabaseEntities())
+            {
+                if (receipt != null)
+                {
+                    var u = db.Stocks.Find(stock.itemNo);
+                    
+                    u.stock1 = u.stock1 + stock.stock1;
+                    db.SaveChanges();
+                    ViewBag.stock = u;
+                    ModelState.AddModelError(string.Empty, PatisserieCestBon.Properties.Settings.Default.p019_info_UpdateSuccess);
+                    return Redirect("StockList");
+                }
+                if(decrease != null)
+                {
+                    var u = db.Stocks.Find(stock.itemNo);
+                    if (u.stock1 == 0)
+                    {
+                        ModelState.AddModelError(string.Empty, PatisserieCestBon.Properties.Settings.Default.p020_error_StockAlreadyZero);
+                        ViewBag.stock = u;
+                        return View("StockUpdate1", stock);
+                    }
+                    if (u.stock1 < stock.stock1)
+                    {
+                        ModelState.AddModelError(string.Empty, PatisserieCestBon.Properties.Settings.Default.p020_error_UpdateStock);
+                        ViewBag.stock = u;
+                        return View("StockUpdate1", stock);
+                    }
+                    else
+                    {
+                        u.stock1 = u.stock1 - stock.stock1;
+                        db.SaveChanges();
+                        ViewBag.stock = u;
+                        return Redirect("StockList");
+                    }
+                }
+                if (update != null)
+                {
+                    var u = db.Stocks.Find(stock.itemNo);
+                    u.receiptDate = stock.receiptDate;
+                    db.SaveChanges();
+                    ViewBag.stock = u;
+                    return Redirect("StockList");
+                }
+                return Redirect("StockList");
             }
         }
     }
